3409df15df8acd92c41c0fb51dc985603a0f5e24
GuadalupeBlancas <78801031+GuadalupeBlancas@users.noreply.github.com>
2022-10-15 11:01:52 -0500

    Import example

---
+++ Python Exercises/import example/main.py
@@ -0,0 +1,11 @@
+import operaciones as o
+
+def main():
+    print(o.suma(8,2))
+    print(o.resta(8,2))
+    print(o.multi(8,2))
+    print(o.div(8,2))
+
+
+if __name__ == '__main__':
+        main()
